90fa77b0e9b764f0be65f8c2671c7ff29f0ba787
Ghai <yusupovgz@yandex.ru>
2020-08-20 22:45:05 +0500

    Site footer complete

--- public_html/index.php
+++ public_html/index.php
@@ -281,7 +281,7 @@ http://www.templatemo.com/tm-477-puzzle
                             <div class="social-icons sticky-sidebar-social">
                                 <nav id="social" class="social-icons"><ul id="menu-social-items" class="social-menu"><li id="menu-item-1734" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1734"><a href="https://www.facebook.com/colorlib"><i class="social_icon fa fa-facebook"><span>Facebook</span></i></a></li>
                                 <li id="menu-item-1735" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1735"><a href="https://twitter.com/colorlib"><i class="social_icon fa fa-twitter"><span>Twitter</span></i></a></li>
-                                <li id="menu-item-1736" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1736"><a href="https://www.youtube.com/channel/UCOaovjLNXdIch2vLFsw_uew"><i class="social_icon fa fa-youtube-play"><span>youtube</span></i></a></li>
+                                <li id="menu-item-1736" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1736"><a href="https://www.youtube.com/channel/UCOaovjLNXdIch2vLFsw_uew"><i class="social_icon fa fa-youtube"><span>youtube</span></i></a></li>
                                 <li id="menu-item-1737" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1737"><a href="https://plus.google.com/100289203607749737039"><i class="social_icon fa fa-google-plus"><span>Google+</span></i></a></li>
                                 <li id="menu-item-1738" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1738"><a href="https://instagram.com"><i class="social_icon fa fa-instagram"><span>Instagram</span></i></a></li>
                                 <li id="menu-item-1739" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1739"><a href="https://github.com/puikinsh/"><i class="social_icon fa fa-github"><span>Github</span></i></a></li>
@@ -315,13 +315,23 @@ http://www.templatemo.com/tm-477-puzzle
 
     </div> <!-- container -->
 
-
-    <div class="text-center footer">
-        <div class="container">
-            Copyright @ 2015 Company Name
-        </div>
+    <div id="footer-area">
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="site-info container" id="contact">
+            <div class="row">
+            <nav id="social" class="social-icons"><ul id="menu-social-items" class="social-menu"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1734"><a href="https://www.facebook.com/colorlib"><i class="social_icon fa fa-facebook"><span>Facebook</span></i></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1735"><a href="https://twitter.com/colorlib"><i class="social_icon fa fa-twitter"><span>Twitter</span></i></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1736"><a href="https://www.youtube.com/channel/UCOaovjLNXdIch2vLFsw_uew"><i class="social_icon fa fa-youtube"><span>youtube</span></i></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1737"><a href="https://plus.google.com/100289203607749737039"><i class="social_icon fa fa-google-plus"><span>Google+</span></i></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1738"><a href="https://instagram.com"><i class="social_icon fa fa-instagram"><span>Instagram</span></i></a></li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1739"><a href="https://github.com/puikinsh/"><i class="social_icon fa fa-github"><span>Github</span></i></a></li>
+            </ul></nav> <div class="copyright col-md-12">
+            Activello. Theme by <a href="https://colorlib.com/" target="_blank">Colorlib</a> Powered by <a href="https://wordpress.org/" target="_blank">WordPress</a> </div>
+            </div>
+            </div>
+            <button class="scroll-to-top" style="display: block;"><i class="fa fa-angle-up"></i></button>
+        </footer>
     </div>
-
 
   <script src="<?php echo url_for('assets/js/main.js') ?>" type="text/javascript"></script>
 </body>
