8cd8233061fa427868acea6e77b69486ffa76bc4
Gainulla <yusupovgz@yandex.ru>
2022-02-07 19:25:42 +0500

    htaccess file modified

---
+++ ---HOPCOURSE BACKUP---/db_credentials.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "u176269397_dbuser");
+define("DB_PASS", "#Pass365#");
+define("DB_NAME", "u176269397_dbname");
+
+?>
