e9fc4932c89086d5ed3ac1a6322b3f37f37d8250
Gainulla <yusupovgz@yandex.ru>
2022-02-08 21:37:06 +0500

    Updated

--- src/smtp_settings.php
+++ src/smtp_settings.php
@@ -1,10 +1,10 @@
 <?php
 
-define("SMTP_HOST", "localhost");
-define("SMTP_PORT", "465");
+define("SMTP_HOST", "");
+define("SMTP_PORT", "");
 define("SMTP_USERNAME", "");
 define("SMTP_PASSWORD", "");
 define("SMTP_SECURE", "");
-define('ADMIN_EMAIL', 'yusupovgz@yandex.ru');
+define('ADMIN_EMAIL', "");
 
 ?>
